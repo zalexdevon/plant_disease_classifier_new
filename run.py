9b04859775990b691708c7cc82395ea5784032bf
Minh Tam <tranminhtamtq2004@gmail.com>
2025-03-29 14:16:17 +0700

    adjust params.yaml

--- run.py
+++ run.py
@@ -5,17 +5,13 @@ import sys
 import os
 
 params = read_yaml(Path(PARAMS_FILE_PATH))
-data_transformation = params.data_transformation
 
-model_name = params.model_name
-evaluated_data_transformation = params.evaluated_data_transformation
+model_name = params.model_trainer.model_name
 evaluated_model_name = params.evaluated_model_name
 
 replace_dict = {
-    "${P}": data_transformation,
     "${T}": model_name,
     "${E}": evaluated_model_name,
-    "${PE}": evaluated_data_transformation,
 }
 
 sub_param_for_yaml_file("config_p.yaml", "config.yaml", replace_dict)
