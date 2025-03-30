f00602f68a287f3d5d912e7c8fc1512d0af5c94e
Minh Tam <tranminhtamtq2004@gmail.com>
2025-03-30 17:40:38 +0700

    c

--- src/classifier/config/configuration.py
+++ src/classifier/config/configuration.py
@@ -46,6 +46,8 @@ class ConfigurationManager:
             train_ds_path=config.train_ds_path,
             val_ds_path=config.val_ds_path,
             class_names=class_names,
+            root_dir=config.root_dir,
+            root_logs_dir=config.root_logs_dir,
             best_model_path=config.best_model_path,
             results_path=config.results_path,
             structure_path=config.structure_path,
